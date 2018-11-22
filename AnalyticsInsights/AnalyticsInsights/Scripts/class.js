a34dce39eb1de8d163bc50ca3bf5332d4d870176
Imola <imola.demeter@yahoo.com>
2018-11-22 22:16:55 +0200

    Gender chart and class chart

---
+++ AnalyticsInsights/AnalyticsInsights/Scripts/class.js
@@ -0,0 +1,58 @@
+﻿$(function () {
+    var chart = new Highcharts.Chart({
+        chart:
+        {
+            renderTo: 'class-container',
+            type: 'column'
+        },
+        title: {
+            text: 'Class Chart'
+        },
+        xAxis: {
+            categories: [
+                '9',
+                '10',
+                '11',
+                '12',
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Percentage'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: '9',
+            data: [49.9]
+
+        }, {
+            name: '10',
+            data: [83.6]
+
+        }, {
+            name: '11',
+            data: [48.9]
+
+        }, {
+            name: '12',
+            data: [42.4]
+
+        }]
+    });
+});
---
+++ AnalyticsInsights/AnalyticsInsights/Scripts/gender.js
@@ -0,0 +1,99 @@
+﻿$(function () {
+    var chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'gender-container',
+            type: 'pie',
+
+
+
+        },
+        colors: ['#F08080', '#1A5276'],
+
+        title: {
+            text: 'Gender',
+            style: {
+                color: '#0A0A0A',
+            },
+            fontSize: "18px"
+        },
+
+        plotOptions: {
+            pie: {
+                innerSize: '75%',
+                dataLabels: {
+                    enabled: false
+                }
+
+            }
+        },
+        credits: false,
+        tooltip: {
+            enabled: true,
+            headerFormat: '<span style="font-size:11px">{point.key}</span><br>',
+            pointFormat: '{series.name}: <b>{point.y:.1f}%</b> '
+        },
+
+        series: [{
+            size: '80%',
+            showInLegends: false,
+            name: 'Segment',
+            data: (function () {
+                myData = [27.0, 73.0];
+                var category = ['Male', 'Female'];
+                // the button action
+                //$('#button').click(function () {
+                var mySeries = [];
+                for (var i in category) {
+                    mySeries.push([category[i], myData[i]]);
+                }
+                //var chart1 = $('#gender-container').highcharts();
+                //chart1.series[0].setData(mySeries);
+                return mySeries;
+            }())
+        }],
+
+
+    },
+
+
+        // using
+
+        function (chart) { // on complete
+
+            var xpos = '50%';
+            var ypos = '53%';
+            var circleradius = 102;
+
+            // Render the circle
+            chart.renderer.circle(xpos, ypos, circleradius).attr({
+                fill: 'none',
+            }).add();
+
+            // Render the text
+            var inner_text = '<p>Men</p><br>';
+            var inner_text1 = "<p><b>".concat(myData[0]).concat("%</b></p>");
+            chart.renderer.text(inner_text, 130, 165).css({
+                width: circleradius * 2,
+                color: 'black',
+                fontSize: '25px',
+                textAlign: 'center'
+            }).attr({
+                // why doesn't zIndex get the text in front of the chart?
+
+                zIndex: 999
+            }).add();
+
+            chart.renderer.text(inner_text1, 130, 197).css({
+                width: circleradius * 2,
+                color: 'black',
+                fontSize: '25px',
+                textAlign: 'center'
+            }).attr({
+                // why doesn't zIndex get the text in front of the chart?
+
+                zIndex: 999
+            }).add();
+        });
+
+
+});
