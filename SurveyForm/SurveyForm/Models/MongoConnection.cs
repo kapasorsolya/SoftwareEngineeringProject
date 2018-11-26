a98df8439c67da0b55fd729b08993902aae22021
Oltean-Peter Boroka <erboro97@gmail.com>
2018-11-26 11:45:14 +0200

    save-data-from-mongo-to-msssql-correct

--- SurveyForm/SurveyForm/Models/MongoConnection.cs
+++ SurveyForm/SurveyForm/Models/MongoConnection.cs
@@ -13,7 +13,7 @@ namespace SurveyForm.Models
     {
         private MongoClient client;
         private string line;
-        private IMongoCollection<MongoCollection> collection;
+        private IMongoCollection<Answers> collection;
         private IMongoDatabase db;
         public MongoConnection(string tableName, string databaseName)
         {
@@ -22,42 +22,42 @@ namespace SurveyForm.Models
             // We will create the Survey database if it does not exist, if does, we open it
             db = client.GetDatabase(databaseName);
             // We get the corresponding collection
-            collection = db.GetCollection<MongoCollection>(tableName);
+            collection = db.GetCollection<Answers>(tableName);
         }
 
         public List<BsonDocument> dataFromMongo()
         {
             // we don't need filter
-            var filter = Builders<MongoCollection>.Filter.Empty;
+            var filter = Builders<Answers>.Filter.Empty;
             // we need the following fields
-            var projection = Builders<MongoCollection>.Projection.Include("answer1").Include("answer2").Include("answer3").Include("answer4").Include("answer5").Include("answer6").Include("answer7").Include("answer8");
+            var projection = Builders<Answers>.Projection.Include("grade").Include("profil").Include("live").Include("gender").Include("learn").Include("speciality").Include("where").Include("university");
             //var projection = Builders<MongoCollection>.Projection.Include("question_id").Include("answer_id").Include("answer_text").Include("user_id").Include("AnswerTimeStamp").Include("survey_id");
             // the result variable contains a list with query's result
-            var result = collection.Find<MongoCollection>(filter).Project(projection).ToList();
+            var result = collection.Find<Answers>(filter).Project(projection).ToList();
             return result;
         }
 
-        public void insertMongo (MongoCollection answers)
+        public void insertMongo (Answers answers)
         {
             collection.InsertOne(answers);
         }
 
-        public static IEnumerable<MongoCollection> newSurveyAnswer(MongoCollection answer)
-        {
-            var surveyAnswer = new MongoCollection
-            {
-            answer1 = answer.answer1,
-            answer2 = answer.answer2,
-            answer3=answer.answer3,
-            answer4=answer.answer4,
-            answer5=answer.answer5,
-            answer6=answer.answer6,
-            answer7=answer.answer7,
-            answer8=answer.answer8
-            };
-            var newAnswer = new List<MongoCollection> { surveyAnswer };
-            return newAnswer;
-        }
+        //public static IEnumerable<MongoCollection> newSurveyAnswer(MongoCollection answer)
+        //{
+        //    var surveyAnswer = new MongoCollection
+        //    {
+        //    answer1 = answer.answer1,
+        //    answer2 = answer.answer2,
+        //    answer3=answer.answer3,
+        //    answer4=answer.answer4,
+        //    answer5=answer.answer5,
+        //    answer6=answer.answer6,
+        //    answer7=answer.answer7,
+        //    answer8=answer.answer8
+        //    };
+        //    var newAnswer = new List<MongoCollection> { surveyAnswer };
+        //    return newAnswer;
+        //}
 
 
     }
